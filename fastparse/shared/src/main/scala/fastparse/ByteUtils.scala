6544449176d07c86896a632b717fe2f062fbb2d5
Li Haoyi <haoyi@dropbox.com>
2016-09-09 02:01:26 +0800

    swap out math.log number-length logic with just .toString.length

--- fastparse/shared/src/main/scala/fastparse/ByteUtils.scala
+++ fastparse/shared/src/main/scala/fastparse/ByteUtils.scala
@@ -21,7 +21,7 @@ object ByteUtils{
       s"Cannot show indices $markers outside input bounds 0 -> ${bytes.length}"
     )
 
-    val maxIndexWidth = math.floor(math.max(0, math.log10(bytes.length))).toInt + 1
+    val maxIndexWidth = bytes.length.toString.length
     val gutter = 4
     val output = new StringBuffer
     val emptyGutter = " " * (maxIndexWidth + gutter)
