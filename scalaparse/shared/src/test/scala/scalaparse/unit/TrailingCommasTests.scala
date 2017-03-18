bb9337f97c7e960f0c08739a13892a89714baef0
Dale Wijnand <dale.wijnand@gmail.com>
2017-03-18 14:56:58 +0000

    Add failing neg tests

--- scalaparse/shared/src/test/scala/scalaparse/unit/TrailingCommasTests.scala
+++ scalaparse/shared/src/test/scala/scalaparse/unit/TrailingCommasTests.scala
@@ -154,5 +154,38 @@ object TrailingCommasTests extends TestSuite {
       // Import, ids, ValDcl, VarDcl, VarDef, PatDef use commas, but not inside paren, bracket or brace,
       // so they don't support an optional trailing comma
     }
+
+    'neg {
+      // make sure trailing commas are only supported when multi-line
+      'ArgumentExprs1 - checkNeg("""trait ArgumentExprs1 { f(23, "bar", )(Ev0, Ev1) }""")
+      'ArgumentExprs2 - checkNeg("""trait ArgumentExprs2 { f(23, "bar")(Ev0, Ev1, ) }""")
+      'ArgumentExprs3 - checkNeg("""trait ArgumentExprs3 { new C(23, "bar", )(Ev0, Ev1) }""")
+      'ArgumentExprs4 - checkNeg("""trait ArgumentExprs4 { new C(23, "bar")(Ev0, Ev1, ) }""")
+
+      'Params1 - checkNeg("""trait Params1 { def f(foo: Int, bar: String, )(implicit ev0: Ev0, ev1: Ev1, ) = 1 }""")
+      'Params2 - checkNeg("""trait Params2 { def f(foo: Int, bar: String, )(implicit ev0: Ev0, ev1: Ev1, ) = 1 }""")
+      'ClassParams1 - checkNeg("""trait ClassParams1 { final class C(foo: Int, bar: String, )(implicit ev0: Ev0, ev1: Ev1) }""")
+      'ClassParams2 - checkNeg("""trait ClassParams2 { final class C(foo: Int, bar: String)(implicit ev0: Ev0, ev1: Ev1, ) }""")
+
+      'SimpleExpr - checkNeg("""trait SimpleExpr { (23, "bar", ) }""")
+
+      'TypeArgs - checkNeg("""trait TypeArgs { def f: C[Int, String, ] }""")
+      'TypeParamClause - checkNeg("""trait TypeParamClause { type C[A, B, ] }""")
+      'FunTypeParamClause - checkNeg("""trait FunTypeParamClause { def f[A, B, ] }""")
+
+      'SimpleType - checkNeg("""trait SimpleType { def f: (Int, String, ) }""")
+      'FunctionArgTypes - checkNeg("""trait FunctionArgTypes { def f: (Int, String, ) => Boolean }""")
+
+      'SimplePattern - checkNeg("""trait SimplePattern { val (foo, bar, ) = null: Any }""")
+
+      'ImportSelectors - checkNeg("""trait ImportSelectors { import foo.{ Ev0, Ev1, } }""")
+
+      'Import - checkNeg("""trait Import { import foo.Ev0, foo.Ev1, }""")
+
+      'ValDcl - checkNeg("""trait ValDcl { val foo, bar, = 23 }""")
+      'VarDcl - checkNeg("""trait VarDcl { var foo, bar, = 23 }""")
+      'VarDef - checkNeg("""trait VarDef { var foo, bar, = _ }""")
+      'PatDef - checkNeg("""trait PatDef { val Foo(foo), Bar(bar), = bippy }""")
+    }
   }
 }
