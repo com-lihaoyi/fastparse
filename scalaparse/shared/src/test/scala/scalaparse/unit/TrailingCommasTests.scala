095581a63d6836c96768611c5ac4518cd159081e
Dale Wijnand <dale.wijnand@gmail.com>
2017-03-18 18:52:53 +0000

    Fix neg tests

--- scalaparse/shared/src/test/scala/scalaparse/unit/TrailingCommasTests.scala
+++ scalaparse/shared/src/test/scala/scalaparse/unit/TrailingCommasTests.scala
@@ -157,35 +157,46 @@ object TrailingCommasTests extends TestSuite {
 
     'neg {
       // make sure trailing commas are only supported when multi-line
-      'ArgumentExprs1 - checkNeg("""trait ArgumentExprs1 { f(23, "bar", )(Ev0, Ev1) }""")
-      'ArgumentExprs2 - checkNeg("""trait ArgumentExprs2 { f(23, "bar")(Ev0, Ev1, ) }""")
-      'ArgumentExprs3 - checkNeg("""trait ArgumentExprs3 { new C(23, "bar", )(Ev0, Ev1) }""")
-      'ArgumentExprs4 - checkNeg("""trait ArgumentExprs4 { new C(23, "bar")(Ev0, Ev1, ) }""")
+      'ArgumentExprs1 - checkNeg("""trait ArgumentExprs1 { f(23, "bar", )(Ev0, Ev1) }""", expected1, found1)
+      'ArgumentExprs2 - checkNeg("""trait ArgumentExprs2 { f(23, "bar")(Ev0, Ev1, ) }""", expected1, found1)
+      'ArgumentExprs3 - checkNeg("""trait ArgumentExprs3 { new C(23, "bar", )(Ev0, Ev1) }""", expected1, found1)
+      'ArgumentExprs4 - checkNeg("""trait ArgumentExprs4 { new C(23, "bar")(Ev0, Ev1, ) }""", expected1, found1)
 
-      'Params1 - checkNeg("""trait Params1 { def f(foo: Int, bar: String, )(implicit ev0: Ev0, ev1: Ev1, ) = 1 }""")
-      'Params2 - checkNeg("""trait Params2 { def f(foo: Int, bar: String, )(implicit ev0: Ev0, ev1: Ev1, ) = 1 }""")
-      'ClassParams1 - checkNeg("""trait ClassParams1 { final class C(foo: Int, bar: String, )(implicit ev0: Ev0, ev1: Ev1) }""")
-      'ClassParams2 - checkNeg("""trait ClassParams2 { final class C(foo: Int, bar: String)(implicit ev0: Ev0, ev1: Ev1, ) }""")
+      'Params1 - checkNeg("""trait Params1 { def f(foo: Int, bar: String, )(implicit ev0: Ev0, ev1: Ev1) = 1 }""", expected1, found1)
+      'Params2 - checkNeg("""trait Params2 { def f(foo: Int, bar: String)(implicit ev0: Ev0, ev1: Ev1, ) = 1 }""", expected1, found1)
+      'ClassParams1 - checkNeg("""final class C(foo: Int, bar: String, )(implicit ev0: Ev0, ev1: Ev1)""", expected1, found1)
+      'ClassParams2 - checkNeg("""final class C(foo: Int, bar: String)(implicit ev0: Ev0, ev1: Ev1, )""", expected1, found1)
 
-      'SimpleExpr - checkNeg("""trait SimpleExpr { (23, "bar", ) }""")
+      'SimpleExpr - checkNeg("""trait SimpleExpr { (23, "bar", ) }""", expected1, found1)
 
-      'TypeArgs - checkNeg("""trait TypeArgs { def f: C[Int, String, ] }""")
-      'TypeParamClause - checkNeg("""trait TypeParamClause { type C[A, B, ] }""")
-      'FunTypeParamClause - checkNeg("""trait FunTypeParamClause { def f[A, B, ] }""")
+      'TypeArgs - checkNeg("""trait TypeArgs { def f: C[Int, String, ] }""", expected2, found2)
+      'TypeParamClause - checkNeg("""class C[A, B, ] }""", expected2, found2)
+      'FunTypeParamClause - checkNeg("""trait FunTypeParamClause { def f[A, B, ] }""", expected2, found2)
 
-      'SimpleType - checkNeg("""trait SimpleType { def f: (Int, String, ) }""")
-      'FunctionArgTypes - checkNeg("""trait FunctionArgTypes { def f: (Int, String, ) => Boolean }""")
+      'SimpleType - checkNeg("""trait SimpleType { def f: (Int, String, ) }""", expected1, found1)
+      'FunctionArgTypes - checkNeg("""trait FunctionArgTypes { def f: (Int, String, ) => Boolean }""", expected1, found1)
 
-      'SimplePattern - checkNeg("""trait SimplePattern { val (foo, bar, ) = null: Any }""")
+      'SimplePattern - checkNeg("""trait SimplePattern { val (foo, bar, ) = null: Any }""", expected1, found1)
 
-      'ImportSelectors - checkNeg("""trait ImportSelectors { import foo.{ Ev0, Ev1, } }""")
+      'ImportSelectors - checkNeg("""import foo.{ Ev0, Ev1, }""", expected3, found3)
 
-      'Import - checkNeg("""trait Import { import foo.Ev0, foo.Ev1, }""")
+      'Import - checkNeg("""trait Import { import foo.Ev0, foo.Ev1, }""", expected4, found4)
 
-      'ValDcl - checkNeg("""trait ValDcl { val foo, bar, = 23 }""")
-      'VarDcl - checkNeg("""trait VarDcl { var foo, bar, = 23 }""")
-      'VarDef - checkNeg("""trait VarDef { var foo, bar, = _ }""")
-      'PatDef - checkNeg("""trait PatDef { val Foo(foo), Bar(bar), = bippy }""")
+      'ValDcl - checkNeg("""trait ValDcl { val foo, bar, = 23 }""", expected5, found5)
+      'VarDcl - checkNeg("""trait VarDcl { var foo, bar, = 23 }""", expected5, found5)
+      'VarDef - checkNeg("""trait VarDef { var foo, bar, = _ }""", expected5, found5)
+      'PatDef - checkNeg("""trait PatDef { val Foo(foo), Bar(bar), = bippy }""", expected5, found5)
+
+      val expected1 = """WSChars | Comment | Newline | ")""""
+      val expected2 = """WSChars | Comment | Newline | "]""""
+      val expected3 = """WSChars | Comment | Newline | "}""""
+      val expected4 = "ThisPath | IdPath"
+      val expected5 = "Binding ~ InfixPattern | InfixPattern | VarId"
+      val found1 = ", )"
+      val found2 = ", ]"
+      val found3 = ", }"
+      val found4 = "}"
+      val found5 = "="
     }
   }
 }
