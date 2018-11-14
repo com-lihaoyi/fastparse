13464b746493355b73b8a7858770845728bded0f
Li Haoyi <haoyi.sg@gmail.com>
2018-11-15 01:50:14 +0800

    Basic whitespace-related failure aggregation tests

--- fastparse/test/src/fastparse/FailureTests.scala
+++ fastparse/test/src/fastparse/FailureTests.scala
@@ -1,6 +1,6 @@
 package test.fastparse
 import fastparse._
-import NoWhitespace._
+
 import utest._
 
 object FailureTests extends TestSuite{
@@ -24,6 +24,7 @@ object FailureTests extends TestSuite{
     }
 
     'simple - {
+      import NoWhitespace._
       def check(parser: P[_] => P[_]) = {
         val f @ Parsed.Failure(failureString, index, extra) = parse("d", parser(_))
         val trace = f.trace(true)
@@ -104,111 +105,113 @@ object FailureTests extends TestSuite{
       }
     }
 
+    'misc - {
+      import NoWhitespace._
+      'sep - {
+        def parseB[_: P] = P( "a" | "b" )
+        def parseA[_: P] = P( parseB.rep(sep = ",") ~ "c" )
+        val f1 @ Parsed.Failure(_, _, _) = parse("ad", parseA(_))
 
-    'sep - {
-      def parseB[_: P] = P( "a" | "b" )
-      def parseA[_: P] = P( parseB.rep(sep = ",") ~ "c" )
-      val f1 @ Parsed.Failure(_, _, _) = parse("ad", parseA(_))
+        val trace = f1.trace()
 
-      val trace = f1.trace()
+        assert(trace.groupAggregateString == """("," | "c")""")
 
-      assert(trace.groupAggregateString == """("," | "c")""")
+        val f2 @ Parsed.Failure(_, _, _) = parse("a,d", parseA(_))
+        val trace2 = f2.trace()
 
-      val f2 @ Parsed.Failure(_, _, _) = parse("a,d", parseA(_))
-      val trace2 = f2.trace()
+        // Make sure if the parse fails after the separator and has to backtrack,
+        // we list both the separator and the post-separator parse that failed
+        // since showing the separator alone (which passed) is misleading
+        assert(trace2.groupAggregateString == """("," ~ parseB | "c")""")
+        f2.index
+      }
 
-      // Make sure if the parse fails after the separator and has to backtrack,
-      // we list both the separator and the post-separator parse that failed
-      // since showing the separator alone (which passed) is misleading
-      assert(trace2.groupAggregateString == """("," ~ parseB | "c")""")
-      f2.index
+      'sepCut - {
+        def parseB[_: P] = P( "a" | "b" | "c" )
+        def parseA[_: P] = P( parseB.rep(sep = ","./) ~ "d" )
+        val f1 @ Parsed.Failure(_, _, _) = parse("ax", parseA(_))
+        val trace = f1.trace()
+
+        trace.groupAggregateString ==> """("," | "d")"""
+        f1.index ==> 1
+
+        val f2 @ Parsed.Failure(_, _, _) = parse("a,x", parseA(_))
+        val trace2 = f2.trace()
+
+        trace2.groupAggregateString ==> """("a" | "b" | "c")"""
+        f2.index ==> 2
+      }
+
+      'aggregateInNamedParser - checkOffset(
+        input = "ad",
+        expected = """("b" | "c")""",
+        label = "\"c\"",
+        parser = {
+          def parseB[_: P] = P("a" ~ "b".?)
+          def parseA[_: P] = P(parseB ~ "c")
+          parseA(_)
+        }
+      )
+
+      'manualSep - checkOffset(
+        input = "a,d",
+        expected = "\"c\"",
+        label = "\"c\"",
+        parser = {
+          def parseA[_: P] = P( "a" ~ (("." | ","./) ~ "c").rep ~ "x" )
+          parseA(_)
+        }
+      )
+
+      'sequentialEithers - checkOffset(
+        input = "z",
+        expected = """("a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "x")""",
+        label = "\"x\"",
+        parser = {
+          def parseD[_: P] = P( (("m" | "n") | "o").rep )
+          def parseC[_: P] = P( (("g" | "h") | "i").? )
+          def parseB[_: P] = P( ("a" | "b") | "c" | "" )
+          def parseA[_: P] = P(
+            parseB ~ ("d" | ("e" | "f") | "") ~
+              parseC ~ ("j" | ("k" | "l")).? ~
+              parseD ~ ("p" | ("q" | "r")).rep ~
+              "x"
+          )
+          parseA(_)
+        }
+      )
+      'passingNamedParsersAggregateIsShallow - checkOffset(
+        input = "ad",
+        expected = """(parseB ~ fail | "c")""",
+        label = "\"c\"",
+        terminals = "\"c\"",
+        parser = {
+          def parseB[_: P] = P( "a" ~ "b".? )
+          def parseA[_: P] = P( (parseB ~ Fail).? ~ "c" )
+          parseA(_)
+        }
+      )
+
+      'passingNamedParsersEitherAggregateIsShallow - checkOffset(
+        input = "_",
+        expected = """(parseB | parseZ)""",
+        label = "(parseB | parseZ)",
+        terminals = """("c" | "d" | "x" | "y")""",
+        parser = {
+          def parseD[_: P] = P("d")
+          def parseC[_: P] = P("c")
+          def parseX[_: P] = P("x")
+          def parseY[_: P] = P("y")
+          def parseB[_: P] = P(parseC | parseD)
+          def parseZ[_: P] = P(parseX | parseY)
+          def parseA[_: P] = P(parseB | parseZ)
+          parseA(_)
+        }
+      )
     }
-
-    'sepCut - {
-      def parseB[_: P] = P( "a" | "b" | "c" )
-      def parseA[_: P] = P( parseB.rep(sep = ","./) ~ "d" )
-      val f1 @ Parsed.Failure(_, _, _) = parse("ax", parseA(_))
-      val trace = f1.trace()
-
-      trace.groupAggregateString ==> """("," | "d")"""
-      f1.index ==> 1
-
-      val f2 @ Parsed.Failure(_, _, _) = parse("a,x", parseA(_))
-      val trace2 = f2.trace()
-
-      trace2.groupAggregateString ==> """("a" | "b" | "c")"""
-      f2.index ==> 2
-    }
-
-    'aggregateInNamedParser - checkOffset(
-      input = "ad",
-      expected = """("b" | "c")""",
-      label = "\"c\"",
-      parser = {
-        def parseB[_: P] = P("a" ~ "b".?)
-        def parseA[_: P] = P(parseB ~ "c")
-        parseA(_)
-      }
-    )
-
-    'manualSep - checkOffset(
-      input = "a,d",
-      expected = "\"c\"",
-      label = "\"c\"",
-      parser = {
-        def parseA[_: P] = P( "a" ~ (("." | ","./) ~ "c").rep ~ "x" )
-        parseA(_)
-      }
-    )
-
-    'sequentialEithers - checkOffset(
-      input = "z",
-      expected = """("a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "x")""",
-      label = "\"x\"",
-      parser = {
-        def parseD[_: P] = P( (("m" | "n") | "o").rep )
-        def parseC[_: P] = P( (("g" | "h") | "i").? )
-        def parseB[_: P] = P( ("a" | "b") | "c" | "" )
-        def parseA[_: P] = P(
-          parseB ~ ("d" | ("e" | "f") | "") ~
-            parseC ~ ("j" | ("k" | "l")).? ~
-            parseD ~ ("p" | ("q" | "r")).rep ~
-            "x"
-        )
-        parseA(_)
-      }
-    )
-    'passingNamedParsersAggregateIsShallow - checkOffset(
-      input = "ad",
-      expected = """(parseB ~ fail | "c")""",
-      label = "\"c\"",
-      terminals = "\"c\"",
-      parser = {
-        def parseB[_: P] = P( "a" ~ "b".? )
-        def parseA[_: P] = P( (parseB ~ Fail).? ~ "c" )
-        parseA(_)
-      }
-    )
-
-    'passingNamedParsersEitherAggregateIsShallow - checkOffset(
-      input = "_",
-      expected = """(parseB | parseZ)""",
-      label = "(parseB | parseZ)",
-      terminals = """("c" | "d" | "x" | "y")""",
-      parser = {
-        def parseD[_: P] = P("d")
-        def parseC[_: P] = P("c")
-        def parseX[_: P] = P("x")
-        def parseY[_: P] = P("y")
-        def parseB[_: P] = P(parseC | parseD)
-        def parseZ[_: P] = P(parseX | parseY)
-        def parseA[_: P] = P(parseB | parseZ)
-        parseA(_)
-      }
-    )
 
     'offset - {
-
+      import NoWhitespace._
       // Consider cases where the failure happens down some branch of an either,
       // rep, or option, where the branch takes place before traceIndex but the
       // actual failure lines up nicely.
@@ -344,6 +347,7 @@ object FailureTests extends TestSuite{
     }
 
     'downstream - {
+      import NoWhitespace._
       // In the case where one branch fails further in than `traceIndex`, we
       // collect the partial aggregation from that branch in the
       // `failureGroupAggregate` but ignore that branch's downstream failure in
@@ -375,6 +379,46 @@ object FailureTests extends TestSuite{
       'repXLeft -   check{ implicit c => (("a" ~ "b") ~ "c").repX ~ "a" ~/ "d" }
       'repSep -     check{ implicit c => ("a" ~ ("b" ~ "c")).rep(sep = Pass) ~ "a" ~/ "d" }
       'repSepLeft - check{ implicit c => (("a" ~ "b") ~ "c").rep(sep = Pass) ~ "a" ~/ "d" }
+    }
+
+    'whitespace - {
+      import SingleLineWhitespace._
+      'opt3 - checkOffset(
+        input = "a x",
+        expected = """("b" | "c" | "d")""",
+        label = "\"d\"",
+        parser = { implicit c => (("a" ~ "b".?) ~ "c").? ~ "a" ~ "d"}
+      )
+
+      'opt4 - checkOffset(
+        input = "a  x",
+        expected = """("b" | "c" | "d")""",
+        label = "\"d\"",
+        parser = { implicit c => ("a" ~ ("b".? ~ "c")).? ~ "a" ~ "d" }
+      )
+
+      'optEitherRep1 - checkOffset(
+        input = "a   x",
+        expected = """("b" | "c" | "d")""",
+        label = "\"d\"",
+        parser = { implicit c => (("a" ~ "b").rep(1, sep = Pass) | ("a" ~ "c")).? ~ "a" ~ "d" }
+      )
+
+      'opt2 - checkOffset(
+        input = "a  b   x",
+        expected = """("b" ~ "c" | "d")""",
+        label = "\"d\"",
+        terminals = "\"d\"",
+        parser = {implicit c => ("a".! ~ ("b".! ~ "c".!)).? ~ "a".! ~/ "d".!}
+      )
+
+      'optLeft2 - checkOffset(
+        input = "a   b  x",
+        expected = """("b" ~ "c" | "d")""",
+        label = "\"d\"",
+        terminals = "\"d\"",
+        parser = { implicit c => (("a".! ~ "b".!) ~ "c".!).? ~ "a".! ~ "d".! }
+      )
     }
   }
 }
