2bf24fa96d9d41f188b4f44f33eeead278bd79fc
Simon Schenk <simon@schenk-online.net>
2021-10-04 15:00:56 +0200

    Implement P.collect (#251)
    Transforms the result of this parser using the given partial function, failing the parse if the partial function is not defined on the result of the current parser. This is eqivalent to `.filter(f.isDefinedAt).map(f.apply)`.

--- fastparse/src/fastparse/internal/MacroImpls.scala
+++ fastparse/src/fastparse/internal/MacroImpls.scala
@@ -160,6 +160,25 @@ object MacroImpls {
     }
   }
 
+  def collectMacro[T: c.WeakTypeTag, V: c.WeakTypeTag]
+                  (c: Context)
+                  (f: c.Expr[PartialFunction[T, V]]): c.Expr[ParsingRun[V]] = {
+    import c.universe._
+
+    val lhs0 = c.prefix.asInstanceOf[c.Expr[EagerOps[T]]]
+    reify {
+      lhs0.splice.parse0 match {
+        case lhs =>
+          if (!lhs.isSuccess) lhs.asInstanceOf[ParsingRun[V]]
+          else {
+            val this2 = lhs.asInstanceOf[ParsingRun[V]]
+            val f2 = f.splice.andThen(v => this2.successValue = v)
+            f2.applyOrElse(this2.successValue.asInstanceOf[T], {_: T => this2.freshFailure()})
+            this2
+          }
+      }
+    }
+  }
 
   def flatMapXMacro[T: c.WeakTypeTag, V: c.WeakTypeTag]
                   (c: Context)
@@ -716,5 +735,4 @@ object MacroImpls {
                          ctx: c.Expr[ParsingRun[_]]): c.Expr[ParsingRun[R]] = {
     MacroImpls.parsedSequence0[T, V, R](c)(other, true)(s, None, ctx)
   }
-
 }
