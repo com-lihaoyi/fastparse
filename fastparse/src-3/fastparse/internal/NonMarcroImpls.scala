5ffc99ad6ae3ae70eabc2856ea7824de0e0e1e91
ragnar <ragnar@rmgk.de>
2022-05-23 23:59:59 +0200

    remove unused method

--- fastparse/src-3/fastparse/internal/NonMarcroImpls.scala
+++ fastparse/src-3/fastparse/internal/NonMarcroImpls.scala
@@ -133,15 +133,6 @@ object NonMarcroImpls {
     }
   }
 
-  def literalStrNonMacro(s1: String)(ctx1: ParsingRun[Any]): ParsingRun[Unit] = {
-    val index = ctx1.index
-    val res =
-      if (Util.startsWith(ctx1.input, s1, index)) ctx1.freshSuccessUnit(index + s1.length)
-      else ctx1.freshFailure().asInstanceOf[ParsingRun[Unit]]
-    if (ctx1.verboseFailures) ctx1.aggregateTerminal(index, () => Util.literalize(s1))
-    res
-  }
-
   def parsedSequence0[T, V, R](lhs: () => ParsingRun[_])(
       rhs: () => ParsingRun[V],
       cut: Boolean
