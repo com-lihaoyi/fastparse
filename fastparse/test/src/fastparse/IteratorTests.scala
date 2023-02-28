ce102a19dfb8b7a9018d3585a1a487af3b097cba
Li Haoyi <haoyi.sg@gmail.com>
2023-02-28 23:47:15 +0800

    .

--- fastparse/test/src/fastparse/IteratorTests.scala
+++ fastparse/test/src/fastparse/IteratorTests.scala
@@ -25,7 +25,7 @@ object IteratorTests extends TestSuite {
   val tests = Tests {
     test("basic"){
       import NoWhitespace._
-      def p[_p: P] = P( "ab" ~/ "cd".rep().! ~ "ef" | "z" )
+      def p[$: P] = P( "ab" ~/ "cd".rep().! ~ "ef" | "z" )
 
       val Parsed.Success(res, i) = parse(Iterator("ab", "cd", "cd", "cd", "ef"), p(_))
 
@@ -35,7 +35,7 @@ object IteratorTests extends TestSuite {
     test("readable"){
       for(bufferSize <- Range(1, 15)){
         import NoWhitespace._
-        def p[_p: P] = P("ab" ~/ "cd".rep().! ~ "ef" | "z")
+        def p[$: P] = P("ab" ~/ "cd".rep().! ~ "ef" | "z")
 
         val Parsed.Success(res, i) = parse(
           ParserInputSource.FromReadable("abcdcdcdef", bufferSize),
@@ -48,7 +48,7 @@ object IteratorTests extends TestSuite {
 
     test("immediateCutDrop"){
       import NoWhitespace._
-      def p[_p: P] = P( "ab" ~/ "cd" | "z" ).log
+      def p[$: P] = P( "ab" ~/ "cd" | "z" ).log
 
       val input = toInput("abcdef")
       val Parsed.Success(res, i) = parse(input, p(_))
@@ -64,7 +64,7 @@ object IteratorTests extends TestSuite {
         " ".? ~ " ".rep()
       }
 
-      def p[_p: P] = P( "ab" ~/ "cd" | "z" )
+      def p[$: P] = P( "ab" ~/ "cd" | "z" )
 
       val input = toInput("abcdef")
       val Parsed.Success(res, i) = parse(input, p(_))
@@ -78,8 +78,8 @@ object IteratorTests extends TestSuite {
       // Top-level sequences, which are not inside any `|`s or `.rep`s or `.?`s,
       // should dropBuffer immediately after every `~`, even without any cuts
 
-      def p[_p: P] = P( "a" ~ "b" ~ "c")
-      def capt[_p: P] = P( p ~ p ~ p)
+      def p[$: P] = P( "a" ~ "b" ~ "c")
+      def capt[$: P] = P( p ~ p ~ p)
       val input = toInput("abcabcabc")
       val Parsed.Success(res, i) = parse(input, capt(_))
       println(i)
@@ -90,8 +90,8 @@ object IteratorTests extends TestSuite {
       test("capturing"){
         import NoWhitespace._
 
-        def p[_p: P] = P( "a" ~/ "b" ~/ "c")
-        def capt[_p: P] = P( p.! ~ p.! ~ p.!)
+        def p[$: P] = P( "a" ~/ "b" ~/ "c")
+        def capt[$: P] = P( p.! ~ p.! ~ p.!)
         val input = toInput("abcabcabc")
         val Parsed.Success(res, i) = parse(input, capt(_))
         assert(
@@ -104,8 +104,8 @@ object IteratorTests extends TestSuite {
       test("nocut"){
         import NoWhitespace._
 
-        def p[_p: P] = P( "a" ~/ "b" ~/ "c")
-        def nocut[_p: P] = P((NoCut(p) ~ NoCut(p) ~/ NoCut(p)) | "abcd")
+        def p[$: P] = P( "a" ~/ "b" ~/ "c")
+        def nocut[$: P] = P((NoCut(p) ~ NoCut(p) ~/ NoCut(p)) | "abcd")
 
         val input1 = toInput("abcabcabc")
         val Parsed.Success(_, i1) = parse(input1, nocut(_))
@@ -124,9 +124,9 @@ object IteratorTests extends TestSuite {
 
       test("either"){
         import NoWhitespace._
-        def p[_p: P] = P( "a" ~ "b" ~ "c")
-        def either[_p: P] = P( (p ~ End) | ("abc" ~ p ~ End) | ("abcabc" ~ p ~ End))
-        def eitherCutted[_p: P] = P( (p ~ End) | ("abc" ~ p ~ End) | ("abcabc" ~/ p ~ End))
+        def p[$: P] = P( "a" ~ "b" ~ "c")
+        def either[$: P] = P( (p ~ End) | ("abc" ~ p ~ End) | ("abcabc" ~ p ~ End))
+        def eitherCutted[$: P] = P( (p ~ End) | ("abc" ~ p ~ End) | ("abcabc" ~/ p ~ End))
 
         val input1 = toInput("abcabcabc")
         val Parsed.Success(_, i1) = parse(input1, either(_))
@@ -164,9 +164,9 @@ object IteratorTests extends TestSuite {
 
       test("rep"){
         import NoWhitespace._
-        def p[_p: P] = P( "a" ~ "b" ~ "c")
-        def rep[_p: P] = P( (p.rep() ~ "d") | (p.rep() ~ "e") )
-        def repCutted[_p: P] = P( (p.rep() ~ "d") | (p.rep() ~/ "e") )
+        def p[$: P] = P( "a" ~ "b" ~ "c")
+        def rep[$: P] = P( (p.rep() ~ "d") | (p.rep() ~ "e") )
+        def repCutted[$: P] = P( (p.rep() ~ "d") | (p.rep() ~/ "e") )
 
         val input1 = toInput("abcabcabcd")
         val Parsed.Success(_, i1) = parse(input1, rep(_))
@@ -194,10 +194,10 @@ object IteratorTests extends TestSuite {
 
       test("all"){
         import NoWhitespace._
-        def p[_p: P] = P( "a" ~ "b" ~ "c" ~/ "d")
-        def np[_p: P] = NoCut(p)
-        def pp[_p: P] = P( "a" ~ "b" ~ "c" ~ End)
-        def all[_p: P] = P( pp | (np ~/ np) | p ~ "e" | "abded".! )
+        def p[$: P] = P( "a" ~ "b" ~ "c" ~/ "d")
+        def np[$: P] = NoCut(p)
+        def pp[$: P] = P( "a" ~ "b" ~ "c" ~ End)
+        def all[$: P] = P( pp | (np ~/ np) | p ~ "e" | "abded".! )
 
         val input = toInput("abded")
 
@@ -214,9 +214,9 @@ object IteratorTests extends TestSuite {
           " ".? ~~/ " ".repX()
         }
 
-        def a[_p: P] = P( "aaa" )
-        def b[_p: P] = P( "bbb" )
-        def ab[_p: P] = P( a ~ b.? ~~ " " ~~ "ccc" )
+        def a[$: P] = P( "aaa" )
+        def b[$: P] = P( "bbb" )
+        def ab[$: P] = P( a ~ b.? ~~ " " ~~ "ccc" )
 
         val input1 = toInput("aaa   bbb ccc")
         val Parsed.Success(_, i1) = parse(input1, ab(_))
@@ -243,7 +243,7 @@ object IteratorTests extends TestSuite {
 
       test("zeroDrops"){
         import NoWhitespace._
-        def p[_p: P] = P(
+        def p[$: P] = P(
           (("big, " ~ ("another, " ~ ("X".? ~/ "Y".?)) | "small, ") ~ "end") | "other"
         )
         val input = toInput("big, another, end")
@@ -262,7 +262,7 @@ object IteratorTests extends TestSuite {
 
     test("traceFailure"){
       import NoWhitespace._
-      def p[_p: P] = P("[" ~ "]")
+      def p[$: P] = P("[" ~ "]")
 
       // fails under native if run inside the intercept - utest bug?
       val t = scala.util.Try{ parse(Iterator("[", " ", "]"), p(_)).asInstanceOf[Parsed.Failure].extra.traced }
--- pythonparse/src/pythonparse/Lexical.scala
+++ pythonparse/src/pythonparse/Lexical.scala
@@ -10,17 +10,17 @@ import fastparse.NoWhitespace._
 object Lexical {
   import fastparse._
 
-  def kw[_p: P](s: String) = s ~ !(letter | digit | "_")
-  def comment[_p: P] = P( "#" ~ CharsWhile(_ != '\n', 0) )
-  def wscomment[_p: P] = P( (CharsWhileIn(" \n") | Lexical.comment | "\\\n").rep )
-  def nonewlinewscomment[_p: P] = P( (CharsWhileIn(" ") | Lexical.comment | "\\\n").rep )
+  def kw[$: P](s: String) = s ~ !(letter | digit | "_")
+  def comment[$: P] = P( "#" ~ CharsWhile(_ != '\n', 0) )
+  def wscomment[$: P] = P( (CharsWhileIn(" \n") | Lexical.comment | "\\\n").rep )
+  def nonewlinewscomment[$: P] = P( (CharsWhileIn(" ") | Lexical.comment | "\\\n").rep )
 
-  def identifier[_p: P]: P[Ast.identifier] =
+  def identifier[$: P]: P[Ast.identifier] =
     P( (letter|"_") ~ (letter | digit | "_").rep ).!.filter(!keywordList.contains(_)).map(Ast.identifier.apply)
-  def letter[_p: P]     = P( lowercase | uppercase )
-  def lowercase[_p: P]  = P( CharIn("a-z") )
-  def uppercase[_p: P]  = P( CharIn("A-Z") )
-  def digit[_p: P]      = P( CharIn("0-9") )
+  def letter[$: P]     = P( lowercase | uppercase )
+  def lowercase[$: P]  = P( CharIn("a-z") )
+  def uppercase[$: P]  = P( CharIn("A-Z") )
+  def digit[$: P]      = P( CharIn("0-9") )
 
   val keywordList = Set(
     "and",       "del",       "from",      "not",       "while",
@@ -32,46 +32,46 @@ object Lexical {
     "def",       "for",       "lambda",    "try"
   )
 
-  def stringliteral[_p: P]: P[String] = P( stringprefix.? ~ (longstring | shortstring) )
-  def stringprefix[_p: P]: P[Unit] = P(
+  def stringliteral[$: P]: P[String] = P( stringprefix.? ~ (longstring | shortstring) )
+  def stringprefix[$: P]: P[Unit] = P(
     "r" | "u" | "ur" | "R" | "U" | "UR" | "Ur" | "uR" | "b" | "B" | "br" | "Br" | "bR" | "BR"
   )
-  def shortstring[_p: P]: P[String] = P( shortstring0("'") | shortstring0("\"") )
-  def shortstring0[_p: P](delimiter: String) = P( delimiter ~ shortstringitem(delimiter).rep.! ~ delimiter)
-  def shortstringitem[_p: P](quote: String): P[Unit] = P( shortstringchar(quote) | escapeseq )
-  def shortstringchar[_p: P](quote: String): P[Unit] = P( CharsWhile(!s"\\\n${quote(0)}".contains(_)) )
+  def shortstring[$: P]: P[String] = P( shortstring0("'") | shortstring0("\"") )
+  def shortstring0[$: P](delimiter: String) = P( delimiter ~ shortstringitem(delimiter).rep.! ~ delimiter)
+  def shortstringitem[$: P](quote: String): P[Unit] = P( shortstringchar(quote) | escapeseq )
+  def shortstringchar[$: P](quote: String): P[Unit] = P( CharsWhile(!s"\\\n${quote(0)}".contains(_)) )
 
-  def longstring[_p: P]: P[String] = P( longstring0("'''") | longstring0("\"\"\"") )
-  def longstring0[_p: P](delimiter: String) = P( delimiter ~ longstringitem(delimiter).rep.! ~ delimiter)
-  def longstringitem[_p: P](quote: String): P[Unit] = P( longstringchar(quote) | escapeseq | !quote ~ quote.take(1)  )
-  def longstringchar[_p: P](quote: String): P[Unit] = P( CharsWhile(!s"\\${quote(0)}".contains(_)) )
+  def longstring[$: P]: P[String] = P( longstring0("'''") | longstring0("\"\"\"") )
+  def longstring0[$: P](delimiter: String) = P( delimiter ~ longstringitem(delimiter).rep.! ~ delimiter)
+  def longstringitem[$: P](quote: String): P[Unit] = P( longstringchar(quote) | escapeseq | !quote ~ quote.take(1)  )
+  def longstringchar[$: P](quote: String): P[Unit] = P( CharsWhile(!s"\\${quote(0)}".contains(_)) )
 
-  def escapeseq[_p: P]: P[Unit] = P( "\\" ~ AnyChar )
+  def escapeseq[$: P]: P[Unit] = P( "\\" ~ AnyChar )
 
-  def negatable[T, _p: P](p: => P[T])(implicit ev: Numeric[T]) = (("+" | "-").?.! ~ p).map {
+  def negatable[T, $: P](p: => P[T])(implicit ev: Numeric[T]) = (("+" | "-").?.! ~ p).map {
     case ("-", i) => ev.negate(i)
     case (_, i) => i
   }
 
-  def longinteger[_p: P]: P[BigInt] = P( integer ~ ("l" | "L") )
-  def integer[_p: P]: P[BigInt] = negatable[BigInt, Any](P( octinteger | hexinteger | bininteger | decimalinteger))
-  def decimalinteger[_p: P]: P[BigInt] = P( nonzerodigit ~ digit.rep | "0" ).!.map(scala.BigInt(_))
-  def octinteger[_p: P]: P[BigInt] = P( "0" ~ ("o" | "O") ~ octdigit.rep(1).! | "0" ~ octdigit.rep(1).! ).map(scala.BigInt(_, 8))
-  def hexinteger[_p: P]: P[BigInt] = P( "0" ~ ("x" | "X") ~ hexdigit.rep(1).! ).map(scala.BigInt(_, 16))
-  def bininteger[_p: P]: P[BigInt] = P( "0" ~ ("b" | "B") ~ bindigit.rep(1).! ).map(scala.BigInt(_, 2))
-  def nonzerodigit[_p: P]: P[Unit] = P( CharIn("1-9") )
-  def octdigit[_p: P]: P[Unit] = P( CharIn("0-7") )
-  def bindigit[_p: P]: P[Unit] = P( "0" | "1" )
-  def hexdigit[_p: P]: P[Unit] = P( digit | CharIn("a-f", "A-F") )
+  def longinteger[$: P]: P[BigInt] = P( integer ~ ("l" | "L") )
+  def integer[$: P]: P[BigInt] = negatable[BigInt, Any](P( octinteger | hexinteger | bininteger | decimalinteger))
+  def decimalinteger[$: P]: P[BigInt] = P( nonzerodigit ~ digit.rep | "0" ).!.map(scala.BigInt(_))
+  def octinteger[$: P]: P[BigInt] = P( "0" ~ ("o" | "O") ~ octdigit.rep(1).! | "0" ~ octdigit.rep(1).! ).map(scala.BigInt(_, 8))
+  def hexinteger[$: P]: P[BigInt] = P( "0" ~ ("x" | "X") ~ hexdigit.rep(1).! ).map(scala.BigInt(_, 16))
+  def bininteger[$: P]: P[BigInt] = P( "0" ~ ("b" | "B") ~ bindigit.rep(1).! ).map(scala.BigInt(_, 2))
+  def nonzerodigit[$: P]: P[Unit] = P( CharIn("1-9") )
+  def octdigit[$: P]: P[Unit] = P( CharIn("0-7") )
+  def bindigit[$: P]: P[Unit] = P( "0" | "1" )
+  def hexdigit[$: P]: P[Unit] = P( digit | CharIn("a-f", "A-F") )
 
 
-  def floatnumber[_p: P]: P[BigDecimal] = negatable[BigDecimal, Any](P( pointfloat | exponentfloat ))
-  def pointfloat[_p: P]: P[BigDecimal] = P( intpart.? ~ fraction | intpart ~ "." ).!.map(BigDecimal(_))
-  def exponentfloat[_p: P]: P[BigDecimal] = P( (intpart | pointfloat) ~ exponent ).!.map(BigDecimal(_))
-  def intpart[_p: P]: P[BigDecimal] = P( digit.rep(1) ).!.map(BigDecimal(_))
-  def fraction[_p: P]: P[Unit] = P( "." ~ digit.rep(1) )
-  def exponent[_p: P]: P[Unit] = P( ("e" | "E") ~ ("+" | "-").? ~ digit.rep(1) )
+  def floatnumber[$: P]: P[BigDecimal] = negatable[BigDecimal, Any](P( pointfloat | exponentfloat ))
+  def pointfloat[$: P]: P[BigDecimal] = P( intpart.? ~ fraction | intpart ~ "." ).!.map(BigDecimal(_))
+  def exponentfloat[$: P]: P[BigDecimal] = P( (intpart | pointfloat) ~ exponent ).!.map(BigDecimal(_))
+  def intpart[$: P]: P[BigDecimal] = P( digit.rep(1) ).!.map(BigDecimal(_))
+  def fraction[$: P]: P[Unit] = P( "." ~ digit.rep(1) )
+  def exponent[$: P]: P[Unit] = P( ("e" | "E") ~ ("+" | "-").? ~ digit.rep(1) )
 
 
-  def imagnumber[_p: P] = P( (floatnumber | intpart) ~ ("j" | "J") )
+  def imagnumber[$: P] = P( (floatnumber | intpart) ~ ("j" | "J") )
 }
